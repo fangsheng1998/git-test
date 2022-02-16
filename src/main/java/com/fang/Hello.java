4d141bd236c243bf11dd3cd674ba1ead8aa7f690
Fangsheng <yueshengfang@foxmail.com>
2022-02-16 14:32:25 +0800

    first commit

---
+++ src/main/java/com/fang/Hello.java
@@ -0,0 +1,13 @@
+package com.fang;
+
+/**
+ * @Author sheng fang
+ * @Date 2022/2/16
+ * @Time 14:28
+ * @Description :
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello,git!");
+    }
+}
